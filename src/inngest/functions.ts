d39604add8e80fc4a14a2177f2b50f22ab757318
Nilesh Mishra <nileshmishra9987@gmail.com>
2025-10-02 16:18:20 +0530

    06 : E2B sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,24 +1,36 @@
 import { Agent, gemini, createAgent } from "@inngest/agent-kit";
-
+import {Sandbox} from "@e2b/code-interpreter"
+import {getSandbox} from "./utils"
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event ,step }) => {
+
+    //
+    const sandboxId = await step.run("get-sandbox-id" , async () =>{
+      const sandbox = await Sandbox.create("vibe-nextjs-learning");
+      return sandbox.sandboxId;
+    })
     
     // Create a new agent with a system prompt (you can add optional tools, too)
-    const summarizer = createAgent({
+    const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer. You write maintable and read able code. write simple next.js & react snippets",
       model: gemini({ model: 'gemini-2.5-flash'}),
     });
 
-    const { output } = await summarizer.run(
+    const { output } = await codeAgent.run(
     `write the folloing snippet : ${event.data.value}`,
-);
-console.log(output);
+    )
 
-    return { output }; // email tha
-  },
+    const sandboxUrl = await step.run("get-sandbox-url" , async () =>{
+      const sandbox = await getSandbox(sandboxId)
+      const host = sandbox.getHost(3000)
+      return `https://${host}`;
+    })
+
+    return { output , sandboxUrl}; // email tha
+ },
 );
