d90f51640621eb793f681ff66b2156f0a96600f3
Nilesh Mishra <nileshmishra9987@gmail.com>
2025-11-09 17:07:08 +0530

    08 : user and agent response save in database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,19 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
+import { json } from "zod";
 
 
 const page = () => {
   const [value , setValue]=useState("");
 
   const trpc = useTRPC()
-  const callingfun= useMutation(trpc.invork.mutationOptions({
+  const {data : messages} = useQuery(trpc.messages.getMany.queryOptions())
+
+  const callingfun= useMutation(trpc.messages.create.mutationOptions({
     onSuccess: () => {
       toast.success("Background job started");
     },
@@ -23,6 +26,7 @@ const page = () => {
       <Button disabled={callingfun.isPending} onClick={ ()=>{callingfun.mutate({value:value})}}> 
         calling inggest function 
       </Button>
+      {JSON.stringify(messages,null,2)}
     </div>
   );
 }
--- src/trpc/prompt.ts
+++ src/trpc/prompt.ts
@@ -1,5 +1,5 @@
 export const PROMPT = `
-You are a senior software engineer working in a sandboxed Next.js 15.5.4 environment.
+You are a senior software engineer working in a sandboxed Next.js 15.3.3 environment.
 
 Environment:
 - Writable file system via createOrUpdateFiles
@@ -20,7 +20,6 @@ Environment:
 - Never use "@" inside readFiles or other file system operations — it will fail
 
 File Safety Rules:
-
 - ALWAYS add "use client" to the TOP, THE FIRST LINE of app/page.tsx and any other relevant files which use browser APIs or react hooks
 
 Runtime Execution (Strict Rules):
@@ -113,23 +112,3 @@ Created a blog layout with a responsive sidebar, a dynamic list of articles, and
 
 This is the ONLY valid way to terminate your task. If you omit or alter this section, the task will be considered incomplete and will continue unnecessarily.
 `;
-
-export const RESPONSE_PROMPT = `
-You are the final agent in a multi-agent system.
-Your job is to generate a short, user-friendly message explaining what was just built, based on the <task_summary> provided by the other agents.
-The application is a custom Next.js app tailored to the user's request.
-Reply in a casual tone, as if you're wrapping up the process for the user. No need to mention the <task_summary> tag.
-Your message should be 1 to 3 sentences, describing what the app does or what was changed, as if you're saying "Here's what I built for you."
-Do not add code, tags, or metadata. Only return the plain text response.
-`
-
-export const FRAGMENT_TITLE_PROMPT = `
-You are an assistant that generates a short, descriptive title for a code fragment based on its <task_summary>.
-The title should be:
-  - Relevant to what was built or changed
-  - Max 3 words
-  - Written in title case (e.g., "Landing Page", "Chat Widget")
-  - No punctuation, quotes, or prefixes
-
-Only return the raw title.
-`
