afa48deedf1ddb1b1452f9f5b458ce86757d44a9
Nilesh Mishra <nileshmishra9987@gmail.com>
2025-11-11 14:03:13 +0530

    09A - architecture

---
+++ src/modules/projects/ui/views/project-view.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+interface Props {
+  projectId: string;
+};
+
+export const ProjectView = ({ projectId }: Props) => {
+  const trpc = useTRPC();
+  const { data: project } = useSuspenseQuery(trpc.projects.getOne.queryOptions({
+    id: projectId,
+  }))
+
+  const { data: messages } = useSuspenseQuery(trpc.messages.getMany.queryOptions({
+    projectId: projectId,
+  }))
+
+  return (
+    <div>
+      {JSON.stringify(project, null, 2)}
+      {JSON.stringify(messages, null, 2)}
+    </div>
+  );
+};
