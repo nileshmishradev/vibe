9ae79f894671605e454183ab2e50047222503986
Nilesh Mishra <nileshmishra9987@gmail.com>
2025-11-27 19:13:26 +0530

    13:worked on code view (code file display) imp chp

---
+++ src/components/file-explorer.tsx
@@ -0,0 +1,172 @@
+import { CopyCheckIcon, CopyIcon } from "lucide-react";
+import { useState, useMemo, useCallback, Fragment } from "react";
+import { Hint } from "./hint";
+import { Button } from "./ui/button";
+import { ResizableHandle, ResizablePanelGroup, ResizablePanel } from "./ui/resizable";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis
+} from "./ui/breadcrumb"
+import { CodeView } from "./code-view";
+import { convertFilesToTreeItems } from "@/lib/utils";
+import { TreeView } from "./tree-view";
+
+
+
+type FileCollection = { [path: string]: string };
+
+function getLanguageFromExtension(filename: string): string {
+  const extension = filename.split('.').pop()?.toLowerCase();
+  return extension || 'text';
+}
+
+
+interface FileBreadcrumbProps {
+  filePath: string
+}
+
+
+// path of file displayed
+const FileBreadcrumb = ({ filePath }: FileBreadcrumbProps) => {
+  const pathSegments = filePath.split('/');
+  const maxSegments = 4; // imp 
+
+  const renderBreadcrumbItems = () => {
+    if (pathSegments.length <= maxSegments) {
+
+      return pathSegments.map((segment, index) => {
+        const isLast = index === pathSegments.length - 1;
+
+        return (
+          <Fragment key={index}>
+            <BreadcrumbItem>
+              {isLast ? (
+                <BreadcrumbPage className="font-medium">
+                  {segment}
+                </BreadcrumbPage>
+              ) : (
+                <span className="text-muted-foreground">{segment}</span>
+              )}
+            </BreadcrumbItem>
+            {!isLast && <BreadcrumbSeparator />}
+          </Fragment>
+        )
+      })
+    } else {
+      const firstSegment = pathSegments[0];
+      const lastSegment = pathSegments[pathSegments.length - 1];
+      return (
+        <>
+          <BreadcrumbItem>
+            <span className="text-muted-foreground">
+              {firstSegment}
+            </span>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbEllipsis />
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage className="font-medium">
+                {lastSegment}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbItem>
+        </>
+      )
+    }
+  }
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {renderBreadcrumbItems()}
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
+
+
+interface FileExplorerProps {
+  files: FileCollection;
+};
+
+export const FileExplorer = ({
+  files,
+}: FileExplorerProps) => {
+  const [selectedFile, setSelectedFile] = useState<string | null>(() => {
+    const fileKeys = Object.keys(files);
+    return fileKeys.length > 0 ? fileKeys[0] : null;
+  });
+
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = useCallback(() => {
+        if(selectedFile){
+            navigator.clipboard.writeText(files[selectedFile]);
+            setCopied(true);
+            setTimeout(() => {
+                setCopied(false);
+            }, 2000);
+        }
+    }, [selectedFile, files]);
+
+  const treeData = useMemo(() => {
+    return convertFilesToTreeItems(files);
+  }, [files])
+
+  const handleFileSelect = useCallback((filePath: string) => {
+    if (files[filePath]) {
+      setSelectedFile(filePath);
+    }
+  }, [files]);
+
+
+  return (
+
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel defaultSize={30} minSize={30} className="bg-sidebar">
+        <TreeView
+          data={treeData}
+          value={selectedFile}
+          onSelect={handleFileSelect}
+        />
+      </ResizablePanel>
+      <ResizableHandle className="hover:bg-primary transition-colors" />
+      <ResizablePanel defaultSize={70} minSize={50} >
+        {selectedFile && files[selectedFile] ? (
+
+          <div className="h-full w-full flex flex-col">
+            <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
+                <FileBreadcrumb filePath={selectedFile}/> {/* understand */}
+              <Hint text="copy path" side="bottom">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="ml-auto"
+                  onClick={handleCopy}
+                  disabled={copied}
+                >
+                   {copied ? <CopyCheckIcon className="h-4 w-4" /> : <CopyIcon className="h-4 w-4" />}
+                </Button>
+              </Hint>
+            </div>
+            <div className="flex-1 overflow-auto">
+              <CodeView
+                code={files[selectedFile]}
+                language={getLanguageFromExtension(selectedFile)}
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="flex h-full items-center justify-center text-muted-foreground">
+            Select a file to view it&apos;s components
+          </div>
+        )}
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
---
+++ src/components/tree-view.tsx
@@ -0,0 +1,109 @@
+import { TreeItem } from "@/types";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarProvider,
+    SidebarRail,
+} from "./ui/sidebar";
+import { ChevronRightIcon, FileIcon, FolderIcon } from "lucide-react";
+import { Collapsible, CollapsibleTrigger, CollapsibleContent } from "./ui/collapsible";
+
+//2
+
+interface TreeViewProps {
+    data: TreeItem[];
+    value?: string | null;
+    onSelect?: (value: string) => void;
+}
+
+export const TreeView = ({data, value, onSelect}:TreeViewProps) => { 
+    return (
+        <SidebarProvider>
+            <Sidebar collapsible="none" className="w-full">
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {data.map((item, index) => (
+                                    <Tree 
+                                        key={index}
+                                        item={item}
+                                        selectedValue={value}
+                                        onSelect={onSelect}
+                                        parentPath=""
+                                    />
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                </SidebarContent>  
+                <SidebarRail />               
+            </Sidebar>
+        </SidebarProvider>
+    )     
+}
+
+// 1
+interface TreeProps{
+    item: TreeItem
+    selectedValue?: string | null;
+    onSelect?: (value: string) => void;
+    parentPath: string
+}
+
+const Tree = ({item, selectedValue, onSelect, parentPath}:TreeProps) => {
+    const [name, ...items] = Array.isArray(item) ? item: [item];
+    const currentPath = parentPath ? `${parentPath}/${name}` : name;
+    console.log(item);
+    if(!items.length){
+        // it's a file
+        const isSelected = selectedValue === currentPath;
+
+        return(
+            <SidebarMenuButton 
+                isActive={isSelected}
+                className="data-[active=true]:bg-transparent"
+                onClick={() => onSelect?.(currentPath)}
+            >
+                <FileIcon/>
+                <span className="truncate">{name}</span>
+            </SidebarMenuButton>
+        )
+    }
+    
+    return(
+        <SidebarMenuItem>
+            <Collapsible
+                className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+                defaultOpen
+            >
+                <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                        <ChevronRightIcon className="transition-transform" />
+                        <FolderIcon />
+                        <span className="truncate">{name}</span>
+                    </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                    <SidebarMenuSub>
+                        {items.map((subItem, index)=>(
+                            <Tree 
+                                key={index}
+                                item={subItem}
+                                selectedValue={selectedValue}
+                                onSelect={onSelect}
+                                parentPath={currentPath}
+                            />
+                        ))}
+                    </SidebarMenuSub>
+                </CollapsibleContent>
+            </Collapsible>
+        </SidebarMenuItem>
+    )
+}
